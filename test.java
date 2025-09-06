b24a6985fcebe234ebbf7039b271897b1364a59f
c <1372118358@qq.com>
2025-09-07 00:45:27 +0800

    fix: correct a typo in README

---
+++ test.java
@@ -0,0 +1,21 @@
+//public表示一个公开的
+//class表示一个类
+//HelloWorld表示一个类名
+public class HelloWorld{//表示定义一个公开的类，起名HelloWorld
+  //类体中不允许直接编写Java语句（除声明变量之外）
+/*
+	public表示公开的
+	static表示静态的
+	void表示空
+	main表示方法名是main
+	(String[] args)是一个main方法的形式参数列表
+	需要记住的是：以下的方法是一个程序的主方法。是程序的执行入口，是一个固定编写方式。
+*/
+  public static void main （String[] args）{//表示定义一个公开的静态的主方法
+    //方法体
+    //方法体
+
+    //Java语句以";"终止，分号必须是半角分号
+    System.out.println("Hello World!");//向控制台输出语句
+  }
+}
